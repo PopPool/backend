15086ee949ca9ca8af75148098f0431ece87a876
JaesungYoun <jayjoy05@khu.ac.kr>
2024-06-24 20:28:17 +0900

    feat: 관심사 종류 정의

---
+++ src/main/java/com/application/poppool/domain/Interest/enums/InterestType.java
@@ -0,0 +1,24 @@
+package com.application.poppool.domain.Interest.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum InterestType {
+    FASION("패션"),               // 패션
+    LIFESTYLE("라이프스타일"),      // 라이프스타일
+    BEAUTY("뷰티"),               // 뷰티
+    FOOD_COOKING("음식/요리"),     // 음식/요리
+    ART("예술"),                  // 예술
+    PETS("반려동물"),              // 반려동물
+    TRAVEL("여행"),               // 여행
+    ENTERTAINMENT("엔터테인먼트"),  // 엔터테인먼트
+    ANIMATION("애니메이션"),       // 애니메이션
+    KIDS("키즈"),                 // 키즈
+    SPORTS("스포츠"),             // 스포츠
+    GAMES("게임");                 // 게임
+
+    private final String name;
+
+}
