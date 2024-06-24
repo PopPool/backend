52f1e1e246835d82f0c71e7f057c4bb653425772
JaesungYoun <jayjoy05@khu.ac.kr>
2024-06-24 20:29:17 +0900

    feat: 회원-관심사 엔티티 정의

---
+++ src/main/java/com/application/poppool/domain/user/entity/UserInterestEntity.java
@@ -0,0 +1,28 @@
+package com.application.poppool.domain.user.entity;
+
+import com.application.poppool.domain.Interest.entity.InterestEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "user_interest", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"USER_ID", "INTEREST_ID"})
+})
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Builder
+public class UserInterestEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID", nullable = false)
+    private UserEntity userEntity;
+
+    @ManyToOne
+    @JoinColumn(name = "INTEREST_ID", nullable = false)
+    private InterestEntity interestEntity;
+}
