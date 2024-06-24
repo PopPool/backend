25d270baa5be09ca6bcdfc0effac4a1c50a02447
JaesungYoun <jayjoy05@khu.ac.kr>
2024-06-24 20:28:31 +0900

    feat: 관심사 엔티티

---
+++ src/main/java/com/application/poppool/domain/Interest/entity/InterestEntity.java
@@ -0,0 +1,37 @@
+package com.application.poppool.domain.Interest.entity;
+
+
+import com.application.poppool.domain.Interest.enums.InterestType;
+import com.application.poppool.domain.user.entity.UserInterestEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "interest")
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Builder
+public class InterestEntity {
+
+    @Id
+    @Column(name = "INTEREST_ID", unique = true)
+    @Enumerated(EnumType.STRING)
+    private InterestType interestId;
+
+    @Column(name = "INTEREST_NAME")
+    private String interestName;
+
+    @OneToMany(mappedBy = "interestEntity", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private Set<UserInterestEntity> userInterestEntities = new HashSet<>();
+
+    public void addUser(UserInterestEntity userInterestEntity) {
+        userInterestEntities.add(userInterestEntity);
+    }
+
+
+}
