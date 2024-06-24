cc1ddaf39eee985ab51bd48bbb1d7f4dd176a9b4
JaesungYoun <jayjoy05@khu.ac.kr>
2024-06-24 20:27:50 +0900

    feat: 전역 예외 처리 및 에러 코드 정의

---
+++ src/main/java/com/application/poppool/global/exception/ExceptionResponse.java
@@ -0,0 +1,17 @@
+package com.application.poppool.global.exception;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class ExceptionResponse {
+    // 에러 코드
+    int code;
+    // 에러 메시지
+    String message;
+
+}
