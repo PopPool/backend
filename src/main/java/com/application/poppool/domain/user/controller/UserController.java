7b3780eb253309e9f739f00607755e3dd741962f
JaesungYoun <jayjoy05@khu.ac.kr>
2024-06-26 04:35:56 +0900

    refactor: 회원가입 기능 Controller 분리로 인한 수정

--- src/main/java/com/application/poppool/domain/user/controller/UserController.java
+++ src/main/java/com/application/poppool/domain/user/controller/UserController.java
@@ -1,12 +1,9 @@
 package com.application.poppool.domain.user.controller;
 
 
-import com.application.poppool.domain.auth.dto.request.SignUpRequest;
 import com.application.poppool.domain.user.service.UserService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -16,24 +13,5 @@ import org.springframework.web.bind.annotation.*;
 public class UserController implements UserControllerDoc{
 
     private final UserService userService;
-
-    /**
-     * 회원가입
-     * @param signUpRequest
-     */
-    @PostMapping("/signup")
-    public void signUp(@RequestBody @Valid SignUpRequest signUpRequest) {
-        log.info("회원가입");
-        userService.signUp(signUpRequest);
-    }
-
-    /**
-     * 닉네임 중복확인
-     */
-    @GetMapping
-    public ResponseEntity<Boolean> checkNickNameDuplicate(@RequestParam String nickName) {
-        log.info("닉네임 중복 확인");
-        return ResponseEntity.ok(userService.isNickNameDuplicate(nickName));
-    }
 
 }
