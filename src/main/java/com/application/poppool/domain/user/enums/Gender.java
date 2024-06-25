a2756856036364ba083c06a71be66bdc0aa8e808
JaesungYoun <jayjoy05@khu.ac.kr>
2024-06-26 04:29:56 +0900

    feat: 성별 label 추가

--- src/main/java/com/application/poppool/domain/user/enums/Gender.java
+++ src/main/java/com/application/poppool/domain/user/enums/Gender.java
@@ -1,5 +1,14 @@
 package com.application.poppool.domain.user.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Gender {
-    MALE,FEMALE,NONE
+    MALE("남자"),
+    FEMALE("여자"),
+    NONE("선택하지 않음");
+
+    private final String label;
 }
