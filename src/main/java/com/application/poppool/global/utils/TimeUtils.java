7de57d4e949b95815193e1f171c5ebfa08ab3ee1
JaesungYoun <jayjoy05@khu.ac.kr>
2024-07-01 20:35:12 +0900

    feat: TimeUtils - Date -> LocalDateTime

---
+++ src/main/java/com/application/poppool/global/utils/TimeUtils.java
@@ -0,0 +1,24 @@
+package com.application.poppool.global.utils;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class TimeUtils {
+
+    /**
+     * Date -> LocalDateTime 변환
+     * @param date
+     * @return
+     */
+    public static LocalDateTime convertDateToLocalDateTime(Date date) {
+        if (date == null) {
+            throw new IllegalArgumentException("Date cannot be null");
+        }
+        return Instant.ofEpochMilli(date.getTime())
+                .atZone(ZoneId.systemDefault())
+                .toLocalDateTime();
+    }
+
+}
