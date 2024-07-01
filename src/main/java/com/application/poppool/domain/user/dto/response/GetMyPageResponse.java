4a96835e81bf346bcfe9f222651e5578e116abdf
JaesungYoun <jayjoy05@khu.ac.kr>
2024-07-01 20:38:10 +0900

    feat: 마이페이지 응답 dto

---
+++ src/main/java/com/application/poppool/domain/user/dto/response/GetMyPageResponse.java
@@ -0,0 +1,23 @@
+package com.application.poppool.domain.user.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class GetMyPageResponse {
+
+    private String nickName;
+    private String instagramId;
+    private List<PopUpInfo> popUpInfoList;
+
+    @Getter
+    @Builder
+    public static class PopUpInfo {
+        private Long popUpStoreId;
+        private String popUpStoreName;
+    }
+
+}
