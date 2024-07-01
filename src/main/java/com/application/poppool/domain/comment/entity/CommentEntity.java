f8d6da497deec4cb861c64adc7f2d9bd8e5d96ee
JaesungYoun <jayjoy05@khu.ac.kr>
2024-07-01 20:37:54 +0900

    feat: 코멘트 엔티티

---
+++ src/main/java/com/application/poppool/domain/comment/entity/CommentEntity.java
@@ -0,0 +1,31 @@
+package com.application.poppool.domain.comment.entity;
+
+import com.application.poppool.domain.popup.entity.PopUpStoreEntity;
+import com.application.poppool.domain.user.entity.UserEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "comment")
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Builder
+public class CommentEntity {
+
+    @Id
+    @Column(name = "COMMENT_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(name = "CONTENT")
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private UserEntity user;
+
+    @ManyToOne
+    @JoinColumn(name = "POP_UP_STORE_ID")
+    private PopUpStoreEntity popUpStore;
+}
