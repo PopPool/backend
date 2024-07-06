e4112e3b3792f1443e31d80411770cbb16f43465
JaesungYoun <jayjoy05@khu.ac.kr>
2024-07-06 20:16:28 +0900

    fix: commentId 타입 오류로 인한 수정

--- src/main/java/com/application/poppool/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/application/poppool/domain/comment/repository/CommentRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface CommentRepository extends JpaRepository<CommentEntity,Long> {
-    Optional<CommentEntity> findById(String commentId);
+    Optional<CommentEntity> findById(Long commentId);
 }
