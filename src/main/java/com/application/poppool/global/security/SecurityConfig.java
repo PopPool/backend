6108a171bd6068c5b726efe36a56d32001d13a13
JaesungYoun <jayjoy05@khu.ac.kr>
2024-07-09 04:36:43 +0900

    fix: /signUp/** url filter 제외

--- src/main/java/com/application/poppool/global/security/SecurityConfig.java
+++ src/main/java/com/application/poppool/global/security/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
     private final CustomAccessDeniedHandler customAccessDeniedHandler;
 
     private static final String[] PERMIT_URL = {
-            "/auth/**", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html"
+            "/auth/**", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html", "/signup/**"
     };
 
     private static final String[] GetMethodPermitURL = {
