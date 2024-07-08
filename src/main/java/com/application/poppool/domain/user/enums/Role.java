e1c06eaa39e92274e3d1e22d0c34765c9e2205bb
JaesungYoun <jayjoy05@khu.ac.kr>
2024-07-09 04:33:52 +0900

    fix: BaseEnum 상속

--- src/main/java/com/application/poppool/domain/user/enums/Role.java
+++ src/main/java/com/application/poppool/domain/user/enums/Role.java
@@ -1,13 +1,18 @@
 package com.application.poppool.domain.user.enums;
 
+import com.application.poppool.global.enums.BaseEnum;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public enum Role {
+public enum Role implements BaseEnum {
     USER("ROLE_USER"), ADMIN("ROLE_ADMIN");
 
-    private final String label;
+    private final String value;
 
+    @Override
+    public String getValue() {
+        return null;
+    }
 }
