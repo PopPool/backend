a1eb04995c065cb10472b8ecc8188db66520a2bc
JaesungYoun <jayjoy05@khu.ac.kr>
2024-07-09 04:39:34 +0900

    feat: 탈퇴 설문 항목 enum converter

---
+++ src/main/java/com/application/poppool/global/converter/WithDrawlSurveyConverter.java
@@ -0,0 +1,9 @@
+package com.application.poppool.global.converter;
+
+import com.application.poppool.domain.user.enums.WithDrawlSurvey;
+
+public class WithDrawlSurveyConverter extends EnumToStringConverter<WithDrawlSurvey> {
+    public WithDrawlSurveyConverter() {
+        super(WithDrawlSurvey.class);
+    }
+}
