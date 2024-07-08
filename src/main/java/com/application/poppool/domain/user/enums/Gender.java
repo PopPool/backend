ee437e4a4ebe16386ca46d3050e3423ff355a0db
JaesungYoun <jayjoy05@khu.ac.kr>
2024-07-09 04:32:57 +0900

    fix: 성별 enum String -> enum 상수(Json -> 객체 역직렬화)

--- src/main/java/com/application/poppool/domain/user/enums/Gender.java
+++ src/main/java/com/application/poppool/domain/user/enums/Gender.java
@@ -1,14 +1,28 @@
 package com.application.poppool.domain.user.enums;
 
+import com.application.poppool.global.enums.BaseEnum;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public enum Gender {
-    MALE("남자"),
-    FEMALE("여자"),
-    NONE("선택하지 않음");
+public enum Gender implements BaseEnum {
+    MALE("남성"),
+    FEMALE("여성"),
+    NONE("선택안함");
 
-    private final String label;
+    @JsonValue
+    private final String value;
+
+    @JsonCreator
+    public static Gender fromValueToEnum(String value) {
+        for (Gender gender : Gender.values()) {
+            if (gender.getValue().equals(value)) {
+                return gender;
+            }
+        }
+        return null;
+    }
 }
