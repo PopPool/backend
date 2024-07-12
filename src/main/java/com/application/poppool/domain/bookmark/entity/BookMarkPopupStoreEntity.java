edaf5b6c8166ecb9c8df372d974cd61b042c9d57
jaesungyoun <jaesungyoun@lotte.net>
2024-07-12 16:51:49 +0900

    feat: 찜한 팝업 엔티티

---
+++ src/main/java/com/application/poppool/domain/bookmark/entity/BookMarkPopupStoreEntity.java
@@ -0,0 +1,31 @@
+package com.application.poppool.domain.bookmark.entity;
+
+import com.application.poppool.domain.popup.entity.PopUpStoreEntity;
+import com.application.poppool.domain.user.entity.UserEntity;
+import com.application.poppool.global.audit.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "pop_up_store")
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Builder
+public class BookMarkPopupStoreEntity extends BaseEntity {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private UserEntity user;
+
+    @ManyToOne
+    @JoinColumn(name = "POP_UP_STORE_ID")
+    private PopUpStoreEntity popUpStore;
+
+
+}
--- src/main/java/com/application/poppool/domain/popup/entity/PopUpStoreEntity.java
+++ src/main/java/com/application/poppool/domain/popup/entity/PopUpStoreEntity.java
@@ -1,10 +1,13 @@
 package com.application.poppool.domain.popup.entity;
 
+import com.application.poppool.domain.bookmark.entity.BookMarkPopupStoreEntity;
 import com.application.poppool.domain.comment.entity.CommentEntity;
 import com.application.poppool.global.audit.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -23,11 +26,22 @@ public class PopUpStoreEntity extends BaseEntity {
     @Column(name = "POP_UP_STORE_NAME")
     private String name;
 
-    @Column(name = "DESCRIPTION")
-    private String description;
+    @Column(name = "DESC")
+    private String desc;
+
+    @Column(name = "START_DT")
+    private LocalDateTime startDate;
+
+    @Column(name = "END_DT")
+    private LocalDateTime endDate;
+
+    @Column(name = "ADDRESS")
+    private String address;
 
     @OneToMany(mappedBy = "popUpStore")
     private List<CommentEntity> comments;
 
+    @OneToMany(mappedBy = "popUpStore", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<BookMarkPopupStoreEntity> bookmarkPopUpStores = new ArrayList<>();
 
 }
