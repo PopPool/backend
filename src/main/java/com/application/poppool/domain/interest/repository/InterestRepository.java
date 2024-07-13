53e92d3754c52b17ce6332feb01dbd1c033a9887
JaesungYoun <jayjoy05@khu.ac.kr>
2024-07-13 21:54:22 +0900

    feat: 정렬 조회 추가

--- src/main/java/com/application/poppool/domain/interest/repository/InterestRepository.java
+++ src/main/java/com/application/poppool/domain/interest/repository/InterestRepository.java
@@ -5,10 +5,13 @@ import com.application.poppool.domain.interest.enums.InterestCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface InterestRepository extends JpaRepository<InterestEntity, Long> {
+
+    List<InterestEntity> findAllByOrderByInterestId();
 
     Optional<InterestEntity> findByInterestId(Long interestId);
     Optional<InterestEntity> findByInterestCategory(InterestCategory interestCategory);
