6b8065b7f89219ee625f247b95f1f4a9f34b509b
JaesungYoun <jayjoy05@khu.ac.kr>
2024-07-13 21:53:32 +0900

    feat: 좋아요 엔티티

---
+++ src/main/java/com/application/poppool/domain/like/entity/LikeEntity.java
@@ -0,0 +1,30 @@
+package com.application.poppool.domain.like.entity;
+
+import com.application.poppool.domain.comment.entity.CommentEntity;
+import com.application.poppool.domain.user.entity.UserEntity;
+import com.application.poppool.global.audit.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "like")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Builder
+public class LikeEntity extends BaseEntity {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private UserEntity user;
+
+    @ManyToOne
+    @JoinColumn(name = "COMMENT_ID") //외래 키가 생성될 테이블의 컬럼명을 지정
+    private CommentEntity comment;
+
+}
