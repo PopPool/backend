dda7ec72067990f6bc0c8e7dfeb411a5894fba1f
JaesungYoun <jayjoy05@khu.ac.kr>
2024-07-30 03:59:15 +0900

    remove: LogoutFailureException 삭제

--- src/main/java/com/application/poppool/global/exception/LogoutFailureException.java
+++
@@ -1,7 +0,0 @@
-package com.application.poppool.global.exception;
-
-public class LogoutFailureException extends RuntimeException{
-    public LogoutFailureException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
