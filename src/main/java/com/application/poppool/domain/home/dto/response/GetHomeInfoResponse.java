d233996df121330921f90ce8c62949f0abe33b4b
JaesungYoun <jayjoy05@khu.ac.kr>
2024-07-31 18:06:56 +0900

    feat: home response

--- src/main/java/com/application/poppool/domain/home/dto/response/GetHomeInfoResponse.java
+++ src/main/java/com/application/poppool/domain/home/dto/response/GetHomeInfoResponse.java
@@ -1,4 +1,20 @@
 package com.application.poppool.domain.home.dto.response;
 
+import com.application.poppool.domain.category.enums.Category;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class GetHomeInfoResponse {
+
+    private String nickname;
+
+    @Getter
+    @Builder
+    public static class newPopUpStores {
+        private Category category;
+        private String image;
+    }
+
 }
