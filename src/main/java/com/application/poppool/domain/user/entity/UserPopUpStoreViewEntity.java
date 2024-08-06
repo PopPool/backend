944f336a0783de2de85c9a75e98bebf88af7a578
JaesungYoun <jayjoy05@khu.ac.kr>
2024-08-06 16:36:12 +0900

    feat: 조회 수, 댓글 수, 찜 수 필드

--- src/main/java/com/application/poppool/domain/user/entity/UserPopUpStoreViewEntity.java
+++ src/main/java/com/application/poppool/domain/user/entity/UserPopUpStoreViewEntity.java
@@ -1,10 +1,12 @@
 package com.application.poppool.domain.user.entity;
 
+import com.application.poppool.domain.comment.entity.CommentEntity;
 import com.application.poppool.domain.popup.entity.PopUpStoreEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 
 @Entity
@@ -19,12 +21,24 @@ import java.time.LocalDateTime;
 public class UserPopUpStoreViewEntity {
 
     @Id
-    @Column(name = "ID")
+    @Column(name = "USER_POPUP_STORE_VIEW_ID")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "VIEWED_AT")
     private LocalDateTime viewedAt;
+
+    @Column(name = "VIEW_CNT")
+    @Builder.Default
+    private long viewCount = 0;
+
+    @Column(name = "COMMENT_CNT")
+    @Builder.Default
+    private long commentCount = 0;
+
+    @Column(name = "BOOKMARK_CNT")
+    @Builder.Default
+    private long bookmarkCount = 0;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "USER_ID")
@@ -33,6 +47,9 @@ public class UserPopUpStoreViewEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "POPUP_STORE_ID")
     private PopUpStoreEntity popUpStore;
+
+    @OneToMany(mappedBy = "userPopUpStoreView")
+    private List<CommentEntity> comments;
 
 
 }
