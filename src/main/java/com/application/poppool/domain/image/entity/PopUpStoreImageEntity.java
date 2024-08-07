0f83ae253851d70621352b73572d4ee5e4bda2b3
JaesungYoun <jayjoy05@khu.ac.kr>
2024-08-07 18:02:53 +0900

    feat: 팝업스토어 이미지 엔티티, 대표이미지 여부 삭제

--- src/main/java/com/application/poppool/domain/image/entity/PopUpStoreImageEntity.java
+++ src/main/java/com/application/poppool/domain/image/entity/PopUpStoreImageEntity.java
@@ -2,32 +2,27 @@ package com.application.poppool.domain.image.entity;
 
 import com.application.poppool.domain.popup.entity.PopUpStoreEntity;
 import com.application.poppool.global.audit.BaseEntity;
-import com.application.poppool.global.converter.BooleanToYNConverter;
 import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "images")
+@Table(name = "popup_store_image")
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Builder
-public class ImageEntity extends BaseEntity {
+public class PopUpStoreImageEntity extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "IMAGE_ID")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "POPUP_STORE_ID", nullable = false)
     private PopUpStoreEntity popupStore;
 
     @Column(name = "IMAGE_URL")
     private String url;
-
-    @Column(name = "MAIN_YN")
-    @Convert(converter = BooleanToYNConverter.class)
-    private boolean isMain;
 
 }
