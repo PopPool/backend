f7d0a7ad899afdbe158123e4d2994181e2f25dc0
JaesungYoun <jayjoy05@khu.ac.kr>
2024-08-07 18:09:20 +0900

    feat: 코멘트 작성, 수정, 삭제, 코멘트 이미지 등록/삭제

--- src/main/java/com/application/poppool/domain/comment/service/CommentService.java
+++ src/main/java/com/application/poppool/domain/comment/service/CommentService.java
@@ -1,19 +1,129 @@
 package com.application.poppool.domain.comment.service;
 
+import com.application.poppool.domain.comment.dto.request.CreateCommentRequest;
+import com.application.poppool.domain.comment.dto.request.UpdateCommentRequest;
 import com.application.poppool.domain.comment.entity.CommentEntity;
+import com.application.poppool.domain.comment.repository.CommentRepository;
+import com.application.poppool.domain.image.entity.CommentImageEntity;
+import com.application.poppool.domain.image.repository.CommentImageRepository;
 import com.application.poppool.domain.like.service.LikeService;
+import com.application.poppool.domain.popup.entity.PopUpStoreEntity;
+import com.application.poppool.domain.popup.repository.PopUpStoreRepository;
 import com.application.poppool.domain.user.entity.UserEntity;
+import com.application.poppool.domain.user.repository.UserRepository;
+import com.application.poppool.global.exception.BadRequestException;
+import com.application.poppool.global.exception.ErrorCode;
+import com.application.poppool.global.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
 
     private final LikeService likeService;
+    private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+    private final PopUpStoreRepository popUpStoreRepository;
+    private final CommentImageRepository commentImageRepository;
 
+
+    @Transactional(readOnly = true)
+    public List<CommentEntity> getPopUpStoreComments(Long popUpStoreId) {
+        return commentRepository.findAllCommentsByPopUpStoreWithUser(popUpStoreId);
+    }
+
+    @Transactional
+    public void createComment(CreateCommentRequest request) {
+        UserEntity user = userRepository.findByUserId(request.getUserId())
+                .orElseThrow(() -> new NotFoundException(ErrorCode.USER_NOT_FOUND));
+
+        PopUpStoreEntity popUpStore = popUpStoreRepository.findById(request.getPopUpStoreId())
+                .orElseThrow(() -> new NotFoundException(ErrorCode.POPUP_STORE_NOT_FOUND));
+
+        /** 코멘트 생성 및 저장 */
+        CommentEntity comment = CommentEntity.builder()
+                .user(user)
+                .popUpStore(popUpStore)
+                .content(request.getContent())
+                .build();
+
+
+        /** 코멘트 이미지 엔티티 생성 및 저장 */
+        for (String url : request.getImageUrlList()) {
+            CommentImageEntity commentImage = CommentImageEntity.builder()
+                    .comment(comment)
+                    .url(url)
+                    .build();
+            // 연관관계 편의 메소드(양방향으로 값 셋팅)
+            comment.addImage(commentImage);
+        }
+
+        // 코멘트 저장
+        commentRepository.save(comment);
+    }
+
+    @Transactional
+    public void updateComment(UpdateCommentRequest request) {
+
+        CommentEntity comment = commentRepository.findById(request.getCommentId())
+                .orElseThrow(() -> new NotFoundException(ErrorCode.COMMENT_NOT_FOUND));
+
+        // 코멘트 내용(텍스트) 수정
+        comment.updateComment(request.getContent());
+        
+        
+        // 코멘트 이미지 수정
+        for (UpdateCommentRequest.ImageAction imageAction : request.getImageUrlList()) {
+            switch (imageAction.getActionType()) {
+                case ADD:
+                    // 이미지 추가
+                    CommentImageEntity commentImageToAdd = CommentImageEntity.builder()
+                            .url(imageAction.getImageUrl())
+                            .comment(comment)
+                            .build();
+                    commentImageRepository.save(commentImageToAdd);
+                    break;
+                case DELETE:
+                    // 이미지 삭제
+                    CommentImageEntity commentImageToDelete = commentImageRepository.findById(imageAction.getImageId())
+                            .orElseThrow(() -> new NotFoundException(ErrorCode.COMMENT_IMAGE_NOT_FOUND));
+                    commentImageRepository.delete(commentImageToDelete);
+                    break;
+                case KEEP:
+                    // 아무 작업하지 않음
+                    break;
+            }
+        }
+    }
+
+    @Transactional
+    public void deleteComment(String userId, Long popUpStoreId, Long commentId) {
+        PopUpStoreEntity popUpStore = popUpStoreRepository.findById(popUpStoreId)
+                .orElseThrow(() -> new NotFoundException(ErrorCode.POPUP_STORE_NOT_FOUND));
+
+        CommentEntity comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new NotFoundException(ErrorCode.COMMENT_NOT_FOUND));
+
+        if (!comment.getUser().getUserId().equals(userId)) {
+            throw new BadRequestException(ErrorCode.NOT_MY_COMMENT);
+        }
+
+        /** 코멘트 삭제, CASCADE 설정으로 코멘트와 관련된 이미지들도 모두 삭제됨 */
+        commentRepository.delete(comment);
+    }
+
+    @Transactional(readOnly = true)
     public boolean isCommentLikedByUser(UserEntity user, CommentEntity comment) {
         return likeService.existsByUserAndComment(user, comment);
+    }
+
+    @Transactional(readOnly = true)
+    public long getLikeCount(CommentEntity comment) {
+        return likeService.countByComment(comment);
     }
 
 }
