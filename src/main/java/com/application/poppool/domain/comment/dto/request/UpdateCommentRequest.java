fc4c888fa43affe74c2f8961c23cb798e7fe3428
JaesungYoun <jayjoy05@khu.ac.kr>
2024-08-07 18:05:50 +0900

    feat: 좋아요(도움돼요) 수정 요청 dto

---
+++ src/main/java/com/application/poppool/domain/comment/dto/request/UpdateCommentRequest.java
@@ -0,0 +1,27 @@
+package com.application.poppool.domain.comment.dto.request;
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class UpdateCommentRequest {
+
+    private String userId;
+    private Long popUpStoreId;
+    private Long commentId;
+    private String content;
+    private List<ImageAction> imageUrlList = new ArrayList<>();
+
+    @Getter
+    public static class ImageAction {
+        private Long imageId;
+        private String imageUrl;
+        private ActionType actionType;
+
+        public enum ActionType {
+            ADD,DELETE,KEEP
+        }
+    }
+}
