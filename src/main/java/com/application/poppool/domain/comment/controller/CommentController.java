7c6f2318af93f7e6db8879f6e75c5ee343932661
JaesungYoun <jayjoy05@khu.ac.kr>
2024-08-07 18:00:49 +0900

    feat: 코멘트 작성,수정,삭제 api

---
+++ src/main/java/com/application/poppool/domain/comment/controller/CommentController.java
@@ -0,0 +1,41 @@
+package com.application.poppool.domain.comment.controller;
+
+import com.application.poppool.domain.comment.dto.request.CreateCommentRequest;
+import com.application.poppool.domain.comment.dto.request.UpdateCommentRequest;
+import com.application.poppool.domain.comment.service.CommentService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/comments")
+public class CommentController implements CommentControllerDoc{
+
+    private final CommentService commentService;
+
+    @Override
+    @PostMapping("")
+    public void createComment(@RequestBody @Valid CreateCommentRequest request) {
+        commentService.createComment(request);
+    }
+
+    @Override
+    @PutMapping("")
+    public void updateComment(@RequestBody @Valid UpdateCommentRequest request) {
+        commentService.updateComment(request);
+    }
+
+    @Override
+    @DeleteMapping("")
+    public void deleteComment(@RequestParam(name = "userId") String userId,
+                              @RequestParam(name = "popUpStoreId") Long popUpStoreId,
+                              @RequestParam(name = "commentId") Long commentId) {
+        commentService.deleteComment(userId, popUpStoreId, commentId);
+
+    }
+
+
+}
