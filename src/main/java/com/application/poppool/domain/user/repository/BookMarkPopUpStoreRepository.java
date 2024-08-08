43db35127cd3d624686cc5b970e13bb34f0c07cb
JaesungYoun <jayjoy05@khu.ac.kr>
2024-08-08 17:57:17 +0900

    feat: 찜 여부 체크

--- src/main/java/com/application/poppool/domain/user/repository/BookMarkPopUpStoreRepository.java
+++ src/main/java/com/application/poppool/domain/user/repository/BookMarkPopUpStoreRepository.java
@@ -1,5 +1,6 @@
 package com.application.poppool.domain.user.repository;
 
+import com.application.poppool.domain.popup.entity.PopUpStoreEntity;
 import com.application.poppool.domain.user.entity.BookMarkPopUpStoreEntity;
 import com.application.poppool.domain.user.entity.UserEntity;
 import org.springframework.data.domain.Page;
@@ -9,11 +10,19 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BookMarkPopUpStoreRepository extends JpaRepository<BookMarkPopUpStoreEntity, Long> {
 
-    @Query(value = "SELECT bmps FROM BookMarkPopUpStoreEntity bmps JOIN FETCH bmps.popUpStore WHERE bmps.user = :user ORDER BY bmps.updateDateTime DESC",
+    @Query(value = "SELECT bmps FROM BookMarkPopUpStoreEntity bmps JOIN FETCH bmps.popUpStore WHERE bmps.user = :user ORDER BY bmps.createDateTime DESC",
             countQuery = "SELECT COUNT(bmps) FROM BookMarkPopUpStoreEntity bmps WHERE bmps.user = :user")
     Page<BookMarkPopUpStoreEntity> findBookMarkPopUpStoresByUser(@Param("user") UserEntity user, Pageable pageable);
+
+    boolean existsByUserAndPopUpStore(UserEntity user, PopUpStoreEntity popUpStore);
+
+    /** FindByFK (외래키로 조회) */
+    // findBy + "FK가 참조하는 엔티티명" + "_" + "FK가 참조하는 엔티티의 ID 필드명(첫글자 대문자)"
+    Optional<BookMarkPopUpStoreEntity> findByUser_UserIdAndPopUpStore_Id(String userId, Long popUpStoreId);
 
 }
