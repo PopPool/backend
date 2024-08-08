d4bd10a607f9b8f10df35e2b667dc4a5c3ce11d9
JaesungYoun <jayjoy05@khu.ac.kr>
2024-08-08 18:06:36 +0900

    feat: 엔티티 조회 쿼리 추가

--- src/main/java/com/application/poppool/domain/user/repository/UserPopUpStoreViewRepository.java
+++ src/main/java/com/application/poppool/domain/user/repository/UserPopUpStoreViewRepository.java
@@ -1,6 +1,7 @@
 package com.application.poppool.domain.user.repository;
 
 import com.application.poppool.domain.popup.entity.PopUpStoreEntity;
+import com.application.poppool.domain.user.entity.UserEntity;
 import com.application.poppool.domain.user.entity.UserPopUpStoreViewEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,9 +10,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserPopUpStoreViewRepository extends JpaRepository<UserPopUpStoreViewEntity, Long> {
 
     @Query("SELECT DISTINCT upsv.popUpStore FROM UserPopUpStoreViewEntity upsv WHERE upsv.user.userId = :userId ORDER BY upsv.viewedAt DESC")
     Page<PopUpStoreEntity> findRecentViewPopUpStoresByUserId(@Param("userId") String userId, Pageable pageable);
+
+    Optional<UserPopUpStoreViewEntity> findByUserAndPopUpStore(UserEntity user, PopUpStoreEntity popUpStore);
+
+
 }
