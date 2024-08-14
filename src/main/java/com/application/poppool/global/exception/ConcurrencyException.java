2d4302a348cc85de074b9895efca7ddd34725793
JaesungYoun <jayjoy05@khu.ac.kr>
2024-08-14 14:50:07 +0900

    feat: 동시성 이슈 커스텀 예외 클래스

---
+++ src/main/java/com/application/poppool/global/exception/ConcurrencyException.java
@@ -0,0 +1,14 @@
+package com.application.poppool.global.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class ConcurrencyException extends BaseException{
+
+    private static final HttpStatus HTTP_STATUS = HttpStatus.CONFLICT;
+
+    public ConcurrencyException(ErrorCode errorCode) {
+        super(errorCode, HTTP_STATUS);
+    }
+
+
+}
