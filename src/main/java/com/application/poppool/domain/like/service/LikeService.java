f42a6f489914fcfa31a58497388cdeec3c9a4e72
JaesungYoun <jayjoy05@khu.ac.kr>
2024-08-14 14:49:34 +0900

    feat: 코멘트 좋아요 Count 동시성 이슈 예외 처리

--- src/main/java/com/application/poppool/domain/like/service/LikeService.java
+++ src/main/java/com/application/poppool/domain/like/service/LikeService.java
@@ -7,8 +7,10 @@ import com.application.poppool.domain.like.entity.LikeEntity;
 import com.application.poppool.domain.user.entity.UserEntity;
 import com.application.poppool.domain.user.repository.UserRepository;
 import com.application.poppool.global.exception.BadRequestException;
+import com.application.poppool.global.exception.ConcurrencyException;
 import com.application.poppool.global.exception.ErrorCode;
 import com.application.poppool.global.exception.NotFoundException;
+import jakarta.persistence.OptimisticLockException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,6 +23,11 @@ public class LikeService {
     private final UserRepository userRepository;
     private final CommentRepository commentRepository;
 
+    /**
+     * 좋아요
+     * @param userId
+     * @param commentId
+     */
     @Transactional
     public void likeComment(String userId, Long commentId) {
         UserEntity user = userRepository.findByUserId(userId)
@@ -42,10 +49,29 @@ public class LikeService {
         // 좋아요 저장
         likeRepository.save(like);
 
-        // 코멘트 좋아요 수 + 1
-        comment.incrementLikeCount();
+        /** 동시성 이슈 방지를 위한 retry 및 예외 처리*/
+        int retryCount = 0;
+        int maxRetryCount = 3; // 최대 재시도 횟수
+
+        while (retryCount < maxRetryCount) {
+            try {
+                // 코멘트 좋아요 수 + 1
+                comment.incrementLikeCount();
+                break;
+            } catch (OptimisticLockException e) {
+                retryCount++;
+                if (retryCount >= maxRetryCount) {
+                    throw new ConcurrencyException(ErrorCode.CONCURRENCY_ERROR);
+                }
+            }
+        }
     }
 
+    /**
+     * 좋아요 취소
+     * @param userId
+     * @param commentId
+     */
     @Transactional
     public void unlikeComment(String userId, Long commentId) {
         UserEntity user = userRepository.findByUserId(userId)
@@ -56,21 +82,45 @@ public class LikeService {
 
         LikeEntity like = likeRepository.findByUserAndComment(user, comment)
                 .orElseThrow(() -> new NotFoundException(ErrorCode.DATA_NOT_FOUND));
-        
+
         // 좋아요 취소
         likeRepository.delete(like);
 
-        // 코멘트 좋아요 수 - 1
-        comment.decrementLikeCount();
-        
+        /** 동시성 이슈 방지를 위한 retry 및 예외 처리*/
+        int retryCount = 0;
+        int maxRetryCount = 3; // 최대 재시도 횟수
+
+        while (retryCount < maxRetryCount) {
+            try {
+                // 코멘트 좋아요 수 - 1
+                comment.decrementLikeCount();
+                break;
+            } catch (OptimisticLockException e) {
+                retryCount++;
+                if (retryCount >= maxRetryCount) {
+                    throw new ConcurrencyException(ErrorCode.CONCURRENCY_ERROR);
+                }
+            }
+        }
     }
 
 
+    /**
+     * 좋아요 여부 확인
+     * @param user
+     * @param comment
+     * @return
+     */
     @Transactional(readOnly = true)
     public boolean existsByUserAndComment(UserEntity user, CommentEntity comment) {
         return likeRepository.existsByUserAndComment(user, comment);
     }
 
+    /**
+     * 코멘트에 달린 좋아요 수 count
+     * @param comment
+     * @return
+     */
     @Transactional(readOnly = true)
     public long countByComment(CommentEntity comment) {
         return likeRepository.countByComment(comment);
