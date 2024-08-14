01f9150ab02ec2b5f2a63836215ddd981d343ed8
JaesungYoun <jayjoy05@khu.ac.kr>
2024-08-14 14:42:54 +0900

    feat: PreSignedUrlResponse (복수의 이미지 처리로 인한 응답 Dto 수정)

---
+++ src/main/java/com/application/poppool/domain/file/dto/response/PreSignedUrlResponse.java
@@ -0,0 +1,22 @@
+package com.application.poppool.domain.file.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Builder
+public class PreSignedUrlResponse {
+
+    private List<PreSignedUrl> preSignedUrlList;
+
+    @Getter
+    @Builder
+    public static class PreSignedUrl {
+        private String objectKey;
+        private String preSignedUrl;
+    }
+
+}
