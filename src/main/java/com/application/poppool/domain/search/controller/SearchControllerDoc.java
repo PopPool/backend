139b8b453e5f9e422f004d2035d7a48c1ac49b5c
JaesungYoun <jayjoy05@khu.ac.kr>
2024-08-16 19:49:32 +0900

    feat: 통합 검색 api

---
+++ src/main/java/com/application/poppool/domain/search/controller/SearchControllerDoc.java
@@ -0,0 +1,14 @@
+package com.application.poppool.domain.search.controller;
+
+import com.application.poppool.domain.search.dto.SearchPopUpStoreResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Tag(name = "통합 검색 API")
+public interface SearchControllerDoc {
+
+    @Operation(summary = "통합 검색" , description = "통합 검색을 합니다.")
+    ResponseEntity<SearchPopUpStoreResponse> searchPopUpStore(@RequestParam String query);
+}
