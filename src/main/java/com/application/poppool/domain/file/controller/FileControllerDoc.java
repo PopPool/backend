f0632f4c20cf98b6284e4ab8a6766d4e6e678657
JaesungYoun <jayjoy05@khu.ac.kr>
2024-08-16 20:21:01 +0900

    feat: api 설명 추가

--- src/main/java/com/application/poppool/domain/file/controller/FileControllerDoc.java
+++ src/main/java/com/application/poppool/domain/file/controller/FileControllerDoc.java
@@ -2,6 +2,7 @@ package com.application.poppool.domain.file.controller;
 
 import com.application.poppool.domain.file.dto.request.PreSignedUrlRequest;
 import com.application.poppool.domain.file.dto.response.PreSignedUrlResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -11,8 +12,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Tag(name = "AWS S3 PreSignedUrl API")
 public interface FileControllerDoc {
 
+    @Operation(summary = "AWS S3 PreSignedUrl(업로드) 조회", description = "AWS S3 PreSignedUrl(업로드)을 조회합니다.")
     ResponseEntity<PreSignedUrlResponse> getPreSignedUploadUrl(@RequestBody @Valid PreSignedUrlRequest request);
 
+    @Operation(summary = "AWS S3 PreSignedUrl(다운로드) 조회", description = "AWS S3 PreSignedUrl(다운로드)을 조회합니다." )
     ResponseEntity<PreSignedUrlResponse> getPreSignedDownloadUrl(@RequestBody @Valid PreSignedUrlRequest request);
 
 }
