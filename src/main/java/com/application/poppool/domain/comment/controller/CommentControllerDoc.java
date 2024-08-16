64e6f762b993a0d5dbf328a22cdbccdb3df6af54
JaesungYoun <jayjoy05@khu.ac.kr>
2024-08-16 21:05:49 +0900

    fix: 코멘트 api 설명 수정

--- src/main/java/com/application/poppool/domain/comment/controller/CommentControllerDoc.java
+++ src/main/java/com/application/poppool/domain/comment/controller/CommentControllerDoc.java
@@ -11,13 +11,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Tag(name = "코멘트 API")
 public interface CommentControllerDoc {
 
-    @Operation(summary = "일반 코멘트 작성", description = "일반 코멘트를 작성합니다.")
+    @Operation(summary = "일반/인스타 코멘트 작성", description = "일반/인스타 코멘트를 작성합니다.")
     void createComment(@RequestBody @Valid CreateCommentRequest request);
 
-    @Operation(summary = "일반 코멘트 수정", description = "일반 코멘트를 수정합니다.")
+    @Operation(summary = "일반/인스타 코멘트 수정", description = "일반/인스타 코멘트를 수정합니다.")
     void updateComment(@RequestBody @Valid UpdateCommentRequest request);
 
-    @Operation(summary = "일반 코멘트 삭제", description = "일반 코멘트를 삭제합니다.")
+    @Operation(summary = "일반/인스타 코멘트 삭제", description = "일반/인스타 코멘트를 삭제합니다.")
     void deleteComment(@RequestParam(name = "userId") String userId,
                        @RequestParam(name = "popUpStoreId") Long popUpStoreId,
                        @RequestParam(name = "commentId") Long commentId);
