aa8d24aae8c46d8a842e42a60ca6036adbe4995d
JaesungYoun <jayjoy05@khu.ac.kr>
2024-08-18 21:35:12 +0900

    refactor: json boolean 역직렬화 이슈로 인한 Enum 타입으로 변경

--- src/main/java/com/application/poppool/domain/comment/entity/CommentEntity.java
+++ src/main/java/com/application/poppool/domain/comment/entity/CommentEntity.java
@@ -1,11 +1,12 @@
 package com.application.poppool.domain.comment.entity;
 
+import com.application.poppool.domain.comment.enums.CommentType;
 import com.application.poppool.domain.image.entity.CommentImageEntity;
 import com.application.poppool.domain.like.entity.LikeEntity;
 import com.application.poppool.domain.popup.entity.PopUpStoreEntity;
 import com.application.poppool.domain.user.entity.UserEntity;
 import com.application.poppool.global.audit.BaseEntity;
-import com.application.poppool.global.converter.BooleanToYNConverter;
+import com.application.poppool.global.converter.CommentTypeConverter;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -32,9 +33,9 @@ public class CommentEntity extends BaseEntity {
     @Column(name = "LIKE_COUNT")
     private long likeCount;
 
-    @Column(name = "INSTARGRAM_YN")
-    @Convert(converter = BooleanToYNConverter.class)
-    private boolean isInstagram;
+    @Column(name = "TYPE")
+    @Convert(converter = CommentTypeConverter.class)
+    private CommentType commentType;
 
     @Version // 낙관적 락 (버전 정보로 동시성 이슈 해결)
     private Long version;
