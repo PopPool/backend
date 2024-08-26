f561763c5d62d3a3446c689b93445a303d19f2b4
JaesungYoun <jayjoy05@khu.ac.kr>
2024-08-26 17:35:03 +0900

    feat: 팝업 스토어 수정 요청 dto

---
+++ src/main/java/com/application/poppool/domain/admin/popup/dto/request/UpdatePopUpStoreRequest.java
@@ -0,0 +1,38 @@
+package com.application.poppool.domain.admin.popup.dto.request;
+
+import com.application.poppool.domain.category.enums.Category;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+public class UpdatePopUpStoreRequest {
+
+    private PopUpStore popUpStore;
+    private Location location;
+    private List<String> imagesToAdd;
+    private List<Long> imagesToDelete;
+
+    @Getter
+    public static class PopUpStore {
+        private Long id;
+        private String name;
+        private Category category;
+        private String desc;
+        private String address;
+        private LocalDateTime startDate;
+        private LocalDateTime endDate;
+        private String mainImageUrl;
+        private List<String> imageUrl;
+    }
+
+    @Getter
+    public static class Location {
+        private double latitude;
+        private double longitude;
+        private String markerTitle;
+        private String markerSnippet;
+    }
+
+}
