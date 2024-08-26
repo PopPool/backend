41e55e2b48f12d084e60de4ee715973dfc75cca1
JaesungYoun <jayjoy05@khu.ac.kr>
2024-08-27 03:42:53 +0900

    feat: 차단 관계 존재 여부 체크 기능

--- src/main/java/com/application/poppool/domain/user/repository/BlockedUserRepository.java
+++ src/main/java/com/application/poppool/domain/user/repository/BlockedUserRepository.java
@@ -9,9 +9,12 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface BlockedUserRepository extends JpaRepository<BlockedUserEntity,Long> {
+public interface BlockedUserRepository extends JpaRepository<BlockedUserEntity,Long>, BlockedUserRepositoryCustom {
 
     Optional<BlockedUserEntity> findByUserAndBlockedUser(UserEntity user, UserEntity blockedUser);
     List<BlockedUserEntity> findAllByUser(UserEntity user);
+
+    // 두 사용자의 차단 관계가 존재하는지 체크
+    boolean existsByUserAndBlockedUser(UserEntity user, UserEntity blockedUser);
 
 }
