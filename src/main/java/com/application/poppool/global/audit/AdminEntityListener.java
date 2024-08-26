a24530b8b084c9b708f54b76f90d623bd9c8f005
JaesungYoun <jayjoy05@khu.ac.kr>
2024-08-26 17:37:15 +0900

    refactor: admin 권한 체크 로직 리팩토링

--- src/main/java/com/application/poppool/global/audit/AdminEntityListener.java
+++ src/main/java/com/application/poppool/global/audit/AdminEntityListener.java
@@ -1,12 +1,22 @@
 package com.application.poppool.global.audit;
 
+import com.application.poppool.domain.user.entity.UserEntity;
+import com.application.poppool.domain.user.entity.UserRoleEntity;
+import com.application.poppool.domain.user.repository.UserRepository;
+import com.application.poppool.global.exception.ErrorCode;
+import com.application.poppool.global.exception.NotFoundException;
 import jakarta.persistence.PreUpdate;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
+@RequiredArgsConstructor
 public class AdminEntityListener {
+
+    private final UserRepository userRepository;
 
     @PreUpdate
     public void preUpdate(BaseAdminEntity entity) {
@@ -19,10 +29,15 @@ public class AdminEntityListener {
 
     private boolean isAdmin() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication.getName().equals("ADMIN")) {
-            return true;
-        }
-        return false;
+
+        UserEntity user = userRepository.findByUserId(authentication.getName())
+                .orElseThrow(() -> new NotFoundException(ErrorCode.USER_NOT_FOUND));
+
+        // 해당 유저의 권한 가져오기
+        Set<UserRoleEntity> userRoleList = user.getUserRoles();
+
+        // 관리자 여부 체크
+        return userRoleList.stream().anyMatch(role -> role.getUserRole().name().equals("ADMIN"));
     }
 
 }
