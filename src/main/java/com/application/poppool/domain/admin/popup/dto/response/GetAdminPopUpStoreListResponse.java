9a2c3ae5b0d04db1c84078aef1dc0cfdea4cd042
JaesungYoun <jayjoy05@khu.ac.kr>
2024-09-06 01:28:25 +0900

    fix: admin 팝업 리스트 조회 dto 필드 값 셋팅

--- src/main/java/com/application/poppool/domain/admin/popup/dto/response/GetAdminPopUpStoreListResponse.java
+++ src/main/java/com/application/poppool/domain/admin/popup/dto/response/GetAdminPopUpStoreListResponse.java
@@ -1,10 +1,7 @@
 package com.application.poppool.domain.admin.popup.dto.response;
 
 import com.application.poppool.domain.category.enums.Category;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 
@@ -16,7 +13,7 @@ public class GetAdminPopUpStoreListResponse {
     private int totalPages; // 맞춤 팝업 리스트 페이지 수
     private long totalElements; // 맞춤 팝업 리스트 전체 개수
 
-    @Getter
+    @Data
     @NoArgsConstructor
     @AllArgsConstructor
     public static class PopUpStore {
