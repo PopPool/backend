4a5bca076aeebb35334bf380e251ee4de5af2833
JaesungYoun <jayjoy05@khu.ac.kr>
2024-12-08 01:55:13 +0900

    validation: 회원가입 dto validation 적용

--- src/main/java/com/application/poppool/domain/sign_up/dto/request/SignUpRequest.java
+++ src/main/java/com/application/poppool/domain/sign_up/dto/request/SignUpRequest.java
@@ -2,6 +2,10 @@ package com.application.poppool.domain.sign_up.dto.request;
 
 import com.application.poppool.domain.auth.enums.SocialType;
 import com.application.poppool.domain.user.enums.Gender;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 import java.util.Set;
@@ -9,11 +13,20 @@ import java.util.Set;
 @Getter
 public class SignUpRequest {
 
+    @Schema(description = "회원 닉네임")
+    @NotBlank(message = "닉네임을 입력해주세요.")
     private String nickname;
+    @Schema(description = "회원 성별")
+    @NotNull(message = "성별을 입력해주세요,")
     private Gender gender;
+    @Schema(description = "회원 연령")
     private int age;
+    @Schema(description = "회원 이메일")
     private String socialEmail;
+    @Schema(description = "소셜 타입(카카오/애플)")
     private SocialType socialType;
+    @Schema(description = "카테고리 ID")
+    @NotEmpty(message = "카테고리를 선택해주세요.")
     private Set<Integer> interestCategories;
 
 }
