ca03c0e9f04133fae913b6eecf2f8b1a7bfb1467
JaesungYoun <jayjoy05@khu.ac.kr>
2024-12-08 01:20:00 +0900

    validation: 팝업 생성/수정 Dto Validation 적용

--- src/main/java/com/application/poppool/domain/admin/popup/dto/request/CreatePopUpStoreRequest.java
+++ src/main/java/com/application/poppool/domain/admin/popup/dto/request/CreatePopUpStoreRequest.java
@@ -1,5 +1,6 @@
 package com.application.poppool.domain.admin.popup.dto.request;
 
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -8,17 +9,36 @@ import java.util.List;
 @Getter
 public class CreatePopUpStoreRequest {
 
+    @NotBlank(message = "팝업 스토어 이름을 입력해주세요.")
     private String name;
+    @NotNull(message = "카테고리를 지정해주세요.")
     private Integer categoryId;
     private String desc;
+    @NotBlank(message = "팝업 스토어 주소를 입력해주세요")
     private String address;
+    @NotNull(message = "시작 일자를 입력해주세요.")
     private LocalDateTime startDate;
+    @NotNull(message = "종료 일자를 입력해주세요.")
     private LocalDateTime endDate;
+    @NotBlank(message = "대표 이미지를 등록해주세요.")
     private String mainImageUrl;
     private boolean bannerYn;
     private List<String> imageUrlList;
+    @DecimalMin(value = "-180.0", message = "위도는 -180.0보다 크거나 같아야 합니다.")
+    @DecimalMax(value = "180.0", message = "위도는 180.0보다 작거나 같아야 합니다.")
     private double latitude;
+    @DecimalMin(value = "-180.0", message = "경도는 -180.0보다 크거나 같아야 합니다.")
+    @DecimalMax(value = "180.0", message = "경도는 180.0보다 작거나 같아야 합니다.")
     private double longitude;
     private String markerTitle;
     private String markerSnippet;
+
+    @AssertTrue(message = "종료 일자는 시작 일자보다 이후여야 합니다.")
+    public boolean isStartDateBeforeEndDate() {
+        if (startDate != null && endDate != null) {
+            return startDate.isBefore(endDate);
+        }
+        return true;
+    }
+
 }
