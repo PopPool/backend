12aa41d68dae90dfd6cde19cc2c3407c4a15bd85
JaesungYoun <jayjoy05@khu.ac.kr>
2024-12-08 01:55:02 +0900

    validation: 공지사항 등록/수정 dto validation 적용

--- src/main/java/com/application/poppool/domain/admin/notice/dto/request/UpdateNoticeRequest.java
+++ src/main/java/com/application/poppool/domain/admin/notice/dto/request/UpdateNoticeRequest.java
@@ -1,11 +1,17 @@
 package com.application.poppool.domain.admin.notice.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class UpdateNoticeRequest {
 
+    @Schema(description = "공지사항 제목")
+    @NotBlank(message = "제목을 입력해주세요.")
     private String title;
+    @Schema(description = "공지사항 내용")
+    @NotBlank(message = "내용을 입력해주세요.")
     private String content;
 
 }
