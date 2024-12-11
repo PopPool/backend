c7ad30bd6c47d56c3fc6e7d19e1f71a8af8ff3ad
JaesungYoun <jayjoy05@khu.ac.kr>
2024-12-12 03:52:24 +0900

    feat: 코멘트 SortCode(정렬 조건)

---
+++ src/main/java/com/application/poppool/global/enums/CommentSortCode.java
@@ -0,0 +1,17 @@
+package com.application.poppool.global.enums;
+
+import com.querydsl.core.types.Order;
+import lombok.Getter;
+
+@Getter
+public enum CommentSortCode implements SortCode {
+    MOST_LIKED("likeCount", Order.DESC);  // 좋아요 많은 순
+
+    private final String field;
+    private final Order order;
+
+    CommentSortCode(String field, Order order) {
+        this.field = field;
+        this.order = order;
+    }
+}
