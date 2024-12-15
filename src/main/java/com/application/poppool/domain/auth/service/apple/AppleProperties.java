4fca379527e619f4bd807fb4da85d426010bb439
JaesungYoun <jayjoy05@khu.ac.kr>
2024-12-15 20:49:28 +0900

    feat: apple 환경 변수 추가

---
+++ src/main/java/com/application/poppool/domain/auth/service/apple/AppleProperties.java
@@ -0,0 +1,19 @@
+package com.application.poppool.domain.auth.service.apple;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "oauth.apple")  // "oauth.apple" 프리픽스를 사용
+public class AppleProperties {
+
+    private String clientId;
+    private String teamId;
+    private String keyId;
+    private String authUrl;
+
+}
