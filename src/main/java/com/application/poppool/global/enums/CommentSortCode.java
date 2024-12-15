8fc8790ae4385a840b35869128158058c6cd277a
JaesungYoun <jayjoy05@khu.ac.kr>
2024-12-15 17:16:15 +0900

    feat: 코멘트 정렬 신규순 기준 추가

--- src/main/java/com/application/poppool/global/enums/CommentSortCode.java
+++ src/main/java/com/application/poppool/global/enums/CommentSortCode.java
@@ -5,8 +5,8 @@ import lombok.Getter;
 
 @Getter
 public enum CommentSortCode implements SortCode {
-    MOST_LIKED("likeCount", Order.DESC);  // 좋아요 많은 순
-
+    MOST_LIKED("likeCount", Order.DESC),  // 좋아요 많은 순
+    NEWEST("createDateTime", Order.DESC); // 최근 작성 순
     private final String field;
     private final Order order;
 
