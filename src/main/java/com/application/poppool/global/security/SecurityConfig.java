ac6d78ce015b54e9d827f77762c9f67e00c92c49
JaesungYoun <jayjoy05@khu.ac.kr>
2025-01-03 14:22:27 +0900

    fix: userId 제거로 인한 permit url 변경

--- src/main/java/com/application/poppool/global/security/SecurityConfig.java
+++ src/main/java/com/application/poppool/global/security/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
     private final CustomAccessDeniedHandler customAccessDeniedHandler;
 
     public static final String[] PERMIT_URL = {
-            "/auth/**", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html", "/users/*/my-page", "/health-check","/home",
+            "/auth/**", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html", "/users/my-page", "/health-check","/home",
             "/popup/*/detail","/locations/search", "/popup/open", "/popup/closed", "/search/popup-stores","/sample/**"
     };
 
