eec9b6ce2648d7bdc1b9cc0a793dbadc2313c1a0
JaesungYoun <jayjoy05@khu.ac.kr>
2025-02-05 20:36:02 +0900

    feat: 회원의 코멘트 개수를 팝업스토어 별로 조회한 dto

---
+++ src/main/java/com/application/poppool/domain/user/dto/UserCommentCountByPopUpStore.java
@@ -0,0 +1,16 @@
+package com.application.poppool.domain.user.dto;
+
+import com.application.poppool.domain.popup.entity.PopUpStoreEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserCommentCountByPopUpStore {
+
+    private PopUpStoreEntity popupStore;
+    private long commentCount;
+
+}
