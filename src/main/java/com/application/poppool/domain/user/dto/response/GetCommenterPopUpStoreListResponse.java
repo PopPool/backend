e77c9240e327cbbf91dfa48e1a0b3981539b462f
JaesungYoun <jayjoy05@khu.ac.kr>
2025-02-08 21:47:04 +0900

    feat: dto 클래스명 변경

---
+++ src/main/java/com/application/poppool/domain/user/dto/response/GetCommenterPopUpStoreListResponse.java
@@ -0,0 +1,26 @@
+package com.application.poppool.domain.user.dto.response;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Builder
+public class GetCommenterPopUpStoreListResponse {
+
+    private List<PopUpInfo> popUpInfoList;
+
+    @Getter
+    @Builder
+    public static class PopUpInfo {
+        private Long popUpStoreId;
+        private String popUpStoreName;
+        private String desc;
+        private LocalDateTime startDate;
+        private LocalDateTime endDate;
+        private String address;
+        private boolean closedYn;
+    }
+
+}
